e6a3e4fd165ada4d1d77922f00d81bdf9ae0a3b8
Andy Tran <andy8tran@gmail.com>
2017-09-06 15:36:36 -0400

    implemented CRUD for pics

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,51 @@
 class PicturesController < ApplicationController
 
+
+
   def index
+    @pictures = Picture.all
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    puts params
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to @picture
+    else
+      render 'new'
+    end
+
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    @picture.update(picture_params)
+    redirect_to picture_path
+  end
+
+  def destroy
+
+    @picture = Picture.find(params[:id])
+    @picture.delete
+    redirect_to pictures_path
+
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
   end
 
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,7 @@
 class Picture < ApplicationRecord
-  
+
+  validates :title, :artist, :url, presence: true;
+
+
+
 end
